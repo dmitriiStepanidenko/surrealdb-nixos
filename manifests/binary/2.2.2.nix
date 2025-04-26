7b3d67a899ad22f7ceda410fa3001bc828768667
Dmitrii Stepanidenko <dimitrij.stepanidenko@gmail.com>
2025-04-26 11:59:34 +0300

    bump to 2.2.2

---
+++ manifests/binary/2.2.2.nix
@@ -0,0 +1,4 @@
+{
+  version = "2.2.2";
+  hash = "sha256-FQK3ED5NcfyFFCSzS9mqx+XWPuSVwkUzV103nfEO1Vs=";
+}
--- manifests/default.nix
+++ manifests/default.nix
@@ -3,5 +3,7 @@
   "2_2_0" = import ./binary/2.2.0.nix;
   "2.2.1" = import ./binary/2.2.1.nix;
   "2_2_1" = import ./binary/2.2.1.nix;
-  latest = import ./binary/2.2.1.nix;
+  "2.2.2" = import ./binary/2.2.2.nix;
+  "2_2_2" = import ./binary/2.2.2.nix;
+  latest = import ./binary/2.2.2.nix;
 }
